c452cba0208042a39ecdfac5a3c5000ee501f603
dizwe <dizwe@gmail.com>
2025-09-26 00:51:41 +0900

    feat: 배포를 위한 base 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/malph/">
       <div className="app">
         <Header />
         <main className="main-content">
