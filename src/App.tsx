e8ca743f0e0bf7c8937988997dbe5efd89355819
dizwe <dizwe2716@gmail.com>
2025-09-26 17:07:14 +0900

    feat: 커스텀 도메인 설정하면서 base 변경

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router basename="/malph/">
+    <Router basename="/">
       <div className="app">
         <Header />
         <main className="main-content">
