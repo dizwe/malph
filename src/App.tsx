c1f9a2fd33838f73ee1f9938b8e2daac32d75095
malcolm <MFB@malcolmui-MacBookPro-2.local>
2026-02-02 00:27:15 +0900

    리다이렉트 페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfService from './pages/TermsOfService'
@@ -8,22 +8,31 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import './App.css'
 
+function AppContent() {
+  const location = useLocation()
+  const isLinksPage = location.pathname.startsWith('/links/onerm_log')
+
+  return (
+    <div className="app">
+      {!isLinksPage && <Header />}
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/onerm_log" element={<OnermLogPage />} />
+          <Route path="/links/onerm_log/*" element={<OnermLogLinksPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+        </Routes>
+      </main>
+      {!isLinksPage && <Footer />}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router basename="/">
-      <div className="app">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/onerm_log" element={<OnermLogPage />} />
-            <Route path="/links/onerm_log/*" element={<OnermLogLinksPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   )
 }
